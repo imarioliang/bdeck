922f5cbea37186eea002e86bc73eff531cfe2744
imarioliang <imarioliang@gmail.com>
2026-01-02 09:05:17 +0700

    feat(ui): Restructure dashboard grid for full-width links and dynamic height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,27 @@ import { NotesPane } from "@/components/dashboard/NotesPane";
 export default function Home() {
   return (
     <main className="min-h-screen p-2 md:p-4 bg-white text-black font-mono">
-      <div className="flex flex-col md:grid md:grid-cols-12 md:grid-rows-6 gap-2 md:gap-4 md:h-[calc(100vh-2rem)] border-4 border-black p-2 md:p-4">
-        {/* Left Column: Links and Timers */}
-        <div className="flex flex-col gap-2 md:gap-4 md:col-span-4 md:row-span-6">
-          <Pane title="01_Links" label="/dev/links" className="flex-1 min-h-[300px] md:min-h-0">
+      <div className="flex flex-col md:grid md:grid-cols-12 gap-2 md:gap-4 border-4 border-black p-2 md:p-4">
+        {/* Full Width Row: Links */}
+        <div className="col-span-12">
+          <Pane title="01_Links" label="/dev/links">
             <LinksPane />
           </Pane>
-          <Pane title="02_Timers" label="/dev/timers" className="md:h-1/3 min-h-[200px] md:min-h-0">
+        </div>
+
+        {/* Left Column: Timers */}
+        <div className="md:col-span-4">
+          <Pane title="02_Timers" label="/dev/timers">
             <TimersPane />
           </Pane>
         </div>
 
         {/* Right Column: Todo and Notes */}
-        <div className="flex flex-col gap-2 md:gap-4 md:col-span-8 md:row-span-6">
-          <Pane title="03_Todo" label="/var/tasks" className="flex-1 min-h-[300px] md:min-h-0">
+        <div className="md:col-span-8 flex flex-col gap-4">
+          <Pane title="03_Todo" label="/var/tasks">
             <TodoPane />
           </Pane>
-          <Pane title="04_Notes" label="/tmp/scratchpad" className="md:h-1/2 min-h-[250px] md:min-h-0">
+          <Pane title="04_Notes" label="/tmp/scratchpad">
             <NotesPane />
           </Pane>
         </div>
